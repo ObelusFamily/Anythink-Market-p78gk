d89d71a7e564edf1aabcd60c73fbbdac0d8522ab
Steve Ng <ntc.steve@outlook.com>
2022-05-17 20:21:42 +0800

    Fixed key

--- backend/newrelic.js
+++ backend/newrelic.js
@@ -13,7 +13,7 @@ exports.config = {
   /**
    * Your New Relic license key.
    */
-  license_key: '72c538fb11181f542863c8d6006cf934FFFFNRAL',
+  license_key: 'license_key',
   logging: {
     /**
      * Level at which to log. 'trace' is most useful to New Relic when diagnosing
